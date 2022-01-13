cf8ea9ddbd34cb7650ceb57ae4e355782ba294c7
tlwaller <tlwaller99@gmail.com>
2022-01-13 12:04:59 -0600

    field turns red on invalid input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,32 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if(enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    } else {
+      setIsValid(true);
+      props.onAddGoal(enteredValue);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}} >Course Goal</label>
+        <input style={{backgroundColor: !isValid ? 'rgba(255, 0, 0, 0.185)' : 'white',
+        borderColor: !isValid ? 'red' : 'black'}} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
